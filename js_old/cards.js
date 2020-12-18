4b2a091d23b8668cfad5ae25b72bcc861d69d4e5
Gleb Klimov <info@gleb.at>
2020-12-19 00:10:28 +0300

    Карточки

--- js_old/cards.js
+++ js_old/cards.js
@@ -42,29 +42,42 @@ function loadDreams(url) {
             }
             let getLike = '';
             if (newData[key]['likes'].indexOf(userId) != -1) {
-                getLike = 'fas';
-            } else {
-                getLike = 'far';
+                getLike = 'getLike';
             }
 
             let getDreamId = newData[key]["id"];
             let likesCount = newData[key].likesCount;
-            let cardTemplate = '<li class="project-info-item">\n' +
-                '                    <a class="project-box-link" target="_blank" rel="noopener noreferer" href="#">\n' +
-                '                        <div class="project-img-box">\n' +
-                '                                <img src="' + image + '" alt=""/>\n' +
-                '                            <p class="project-box-desc">\n' + dreamDescrip +
-                '                            </p>\n' +
-                '                        </div>\n' +
-                '                        <div class="project-info-box">\n' +
-                '                            <h5 class="projecthead">' + dreamName + '</h5>\n' +
-                '                            <p class="projectdesc">' + dreamDescrip + '</p>\n' +
-                '                        </div>\n' +
-                '                    </a>\n' +
-                '                </li>'
+
+            let cardTemplate2 = `<li class="project-info-item">
+                    <a class="project-box-link" target="_blank" rel="noopener noreferer" href="#">
+                        <div class="project-img-box">
+                            <img src="${image}" alt=""/>
+                            <p class="project-box-desc">${dreamDescrip}</p>
+                        </div>
+                        <div class="percentscale-container">
+                            <span>100%</span>
+                            <div class="percentscale" style="width: ${percent}%"></div>
+                        </div>
+                        <div class="project-info-box">
+                            <h5 class="projecthead">${dreamName}</h5>
+                            <div class="bottomline">
+                                <p class="projectdesc"><span>${dayCount}</span> days to go <br/> <span>${dollarCurrently}k</span> out of <span>${dollarNeeded}k</span></p>
+                                <button class="cardthumb ${getLike}">
+                                    <span>${likesCount}</span>
+                                    <svg>
+                                        <use href="./img/sprite.svg#thumbs"></use>
+                                    </svg>
+                                </button>
+                            </div>
+
+
+                        </div>
+                    </a>
+                </li>`
+
             // let cardTemplate = '<div class="dream-card"><div class="progress progress-bar-vertical"><div class="progress-bar" role="progressbar" aria-valuenow="' + percent + '" aria-valuemin="0" aria-valuemax="100" style="height: ' + percent + '%;"><span class="sr-only">' + percent + '% Complete</span></div></div><div class="main_info"><a href="donate.html?' + getDreamId + '"><div class="img_card"><img src="' + image + '"alt=""></div></a><div class="detail_info"><div class="dream_title">' + dreamName + '</div><div class="dream_descrip">' + dreamDescrip + '</div><div class="dream_bottom"><div class="dream_percent">' + percent + '%</div><div class="days_people"><div class="dream_days"><span>' + dayCount + '</span> days to go</div><div class="dream_people"><span>' + dollarCurrently + 'k</span> out of <span>' + dollarNeeded + 'k</span></div></div><div class="dream_location"><i class="fas fa-map-marker-alt fa-2x" data-toggle="tooltip" data-placement="bottom" title="' + dreamLocation + '"></i></div><div class="dream_like"><input type="checkbox" class="like__input"><i id="' + getDreamId + '"  title="Likes: ' + likesCount + '" class="' + getLike + ' fa-thumbs-up fa-2x like__heart"></i></div></div></div></div></div>';
 
-            temp += cardTemplate;
+            temp += cardTemplate2;
 
 
             //#endregion
