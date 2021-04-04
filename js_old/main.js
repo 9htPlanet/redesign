d4beb9a4cc112163744bda274108d846a820567a
Gleb Klimov <info@gleb.at>
2021-04-04 15:37:02 +0300

    Some fixes for main page

--- js_old/main.js
+++ js_old/main.js
@@ -1,11 +1,22 @@
-if (isAuth()) {
-    apiGetJson("dreams/my")
-        .then((data) => {
-            if (data.length > 0) {
-                const button = document.getElementById("start_dream_button");
-                button.removeAttribute("data-modal-start-dream-open")
-                button.innerHTML = "Go To My Dream"
-                button.setAttribute("onclick", "location.href='profile.html'")
-            }
-        })
-}
+(() => {
+    const button = document.getElementById("start_dream_button");
+    button.disabled = true;
+    button.innerHTML = "";
+
+    if (isAuth()) {
+        apiGetJson("dreams/my")
+            .then((data) => {
+                if (data.length > 0) {
+                    button.innerHTML = "Go To My Dream";
+                    button.setAttribute("onclick", "location.href='profile.html'");
+                } else {
+                    button.disabled = false;
+                    button.innerHTML = "Start Dream";
+                }
+            })
+    } else {
+        button.disabled = false;
+        button.innerHTML = "Start Dream";
+    }
+
+})()
