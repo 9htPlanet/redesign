02f497339d527b70e4b65100db7c776c658d8ee0
Роман Коваленко <romulus90@mail.ru>
2021-04-17 21:23:33 +0300

    [fix] Откорректирован слайдер, шиммер висит до появления фото

--- js/donate.js
+++ js/donate.js
@@ -90,11 +90,178 @@ function getDreamById(dreamId) {
         });
 }
 
+function showShimmers() {
+    const tabNode = document.querySelector("#tab_investments");
+    tabNode.innerHTML = `<div class="container">
+                            <ul class="projects-info" id="tab_investments_content">
+                                <li class="project-info-item">
+                                    <div class="project-box-link" href="#" rel="noopener noreferer" target="_blank">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="project-info-item">
+                                    <div class="project-box-link" href="#" rel="noopener noreferer" target="_blank">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="project-info-item">
+                                    <div class="project-box-link" href="#" rel="noopener noreferer" target="_blank">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </div>`;
+
+
+    const tabNodeTracing = document.querySelector("#tab_tracing");
+    tabNodeTracing.innerHTML = `<div class="container">
+                            <ul class="projects-info" id="tab_tracing_content">
+                                <li class="project-info-item">
+                                    <div class="project-box-link" rel="noopener noreferer">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="project-info-item">
+                                    <div class="project-box-link" href="#" rel="noopener noreferer" target="_blank">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="project-info-item">
+                                    <div class="project-box-link" href="#" rel="noopener noreferer" target="_blank">
+                                        <div class="project-img-box">
+                                            <span class="shine"></span>
+
+                                        </div>
+
+                                        <div class="project-info-box">
+                                            <h5 class="projecthead">
+                                                <span class="shine h5_shine"></span>
+                                            </h5>
+                                            <div class="bottomline">
+                                                <p class="projectdesc">
+                                                    <span class="shine descr_shine"></span><br/><span
+                                                        class="shine descr_shine"></span>
+                                                </p>
+
+                                                <button class="cardthumb shine"></button>
+                                            </div>
+
+
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+
+                        </div>`;
+}
+
 
 $('document').ready(function () {
-    let getDreamId = window.location.href.toString().split('.html?')[1];
+    let dreamId = window.location.href.toString().split('.html?')[1];
 
-    getDreamById(getDreamId)
+    Dream.getDreamWithShimmerById(dreamId);
+    try {
+        Slider.loadSlider();
+    } catch (e) {
+        console.log("loadSlider error: " + e.toString())
+    }
+
 });
 
 (() => {
