c9e617a2f43b143a0551517f17980f5e468961f2
Роман Коваленко <romulus90@mail.ru>
2021-04-24 17:14:09 +0300

    [add] Добавлен возврат в окно входа после сброса пароля

--- js/resetPassword.js
+++ js/resetPassword.js
@@ -3,24 +3,37 @@ $("document").ready(function () {
     var resetForm = document.getElementById("forgot_id");
     resetForm.addEventListener("submit", function (event) {
         let email = document.getElementById("forgot_email_id").value;
-        const params = {
-            email: email
-        }
-        apiGetJsonQuery("auth/pass", params)
-            .then(function (response) {
-                orderComplete(response)
-            })
+        let btn = document.getElementById("btn_reset_submit");
+        if (btn.innerHTML !== "Back to sign in") {
+            const params = {
+                email: email
+            }
+            apiGetJsonQuery("auth/pass", params)
+                .then(function (response) {
+                    orderComplete(response)
+                })
 
+        }
+        backToSignIn();
         event.preventDefault();
     });
 
 
 })
 
-var orderComplete = function (response) {
+let orderComplete = function (response) {
     if (response) {
         document.querySelector(".result-message-success").classList.remove("hidden");
-        document.getElementById("btn_reset_submit").disabled = true;
+        let btn = document.getElementById("btn_reset_submit");
+        btn.innerHTML = "Back to sign in";
     }
 };
+let backToSignIn = function () {
+    let btn = document.getElementById("btn_reset_submit");
+    if (btn.innerHTML === "Back to sign in") {
+        btn.onclick = toggleModalForgotPassword();
+        btn.onclick = toggleModalLogin();
+    }
+}
+
 
