a6c2596001d1eb28e08837b892c4660ef7b523bb
Gleb Klimov <info@gleb.at>
2021-04-24 23:46:05 +0300

    Change slider, make it working normally

--- js/Slider2.js
+++ js/Slider2.js
@@ -1,137 +1,81 @@
 class Slider {
 
+    static position = 0
+    static imagesCount = 0
+    static sliderLine;
+
     static loadSlider() {
-        const images = document.querySelectorAll('.slider .slider-line img');
-        const sliderLine = document.querySelector('.slider .slider-line');
-        let count = 0;
-        let width;
+        Slider.sliderLine = document.querySelector('.slider .slider-line');
+        Slider.position = 0;
+        Slider.imagesCount = document.querySelectorAll('.slider .slider-line img').length;
 
-        Slider.init(sliderLine, images, count);
-        window.addEventListener('resize', Slider.init);
+        Slider.initPrevious();
+        Slider.initNext();
+        Slider.rollSlider(0)
 
-        Slider.previous();
-        Slider.next();
+        window.addEventListener('resize', Slider.rollSlider);
 
-
-    }
-
-    static init(sliderLine, images, count) {
-        let width = document.querySelector('.slider').offsetWidth;
-        Slider.rollSlider(sliderLine, count, width);
-        Slider.imgByCenter(images, width);
-    }
-
-    static next() {
-        let count1 = 0;
-        const sliderLine1 = document.querySelector('.slider .slider-line');
-        let width1 = document.querySelector('.slider').offsetWidth;
-        const images1 = document.querySelectorAll('.slider .slider-line img');
-
-        document.querySelector('.slider-next').addEventListener('click', function () {
-            count1++;
-            if (count1 >= images1.length) {
-                count1 = 0;
+        document.addEventListener('keydown', function (event) {
+            switch (event.key) {
+                case "ArrowLeft":
+                    Slider.doPrev();
+                    break;
+                case "ArrowRight":
+                    Slider.doNext();
+                    break;
             }
-            Slider.rollSlider(sliderLine1, count1, width1);
         });
+        setTimeout(Slider.rollSlider, 50);
     }
 
-    static previous() {
-        let count1 = 0;
-        const sliderLine1 = document.querySelector('.slider .slider-line');
-        let width1 = document.querySelector('.slider').offsetWidth;
-        const images1 = document.querySelectorAll('.slider .slider-line img');
-
-        document.querySelector('.slider-prev').addEventListener('click', function () {
-            count1--;
-            if (count1 < 0) {
-                count1 = images1.length - 1;
-            }
-            Slider.rollSlider(sliderLine1, count1, width1);
+    static initNext() {
+        document.querySelector('.slider-next').addEventListener('mousedown', function () {
+            Slider.doNext();
         });
-    }
+        document.querySelector('.slider-line').addEventListener('mousedown', function () {
+            Slider.doNext();
+        });
 
-
-    static rollSlider(sliderLine, count, width) {
-        const sliderLine2 = document.querySelector('.slider .slider-line');
-
-        sliderLine2.style.transform = 'translate(-' + count * width + 'px)';
-
-    }
-
-    static imgByCenter(images, width) {
-        if (images != null) {
-            console.log(images.length);
-            for (let i = 0; i < images.length; i++) {
-                let prevDifference;
-                let currentDifference = (width - images[i].width) / 2;
-                if (images[i - 1] == null) {
-                    images[i].style.marginLeft = currentDifference + "px";
-                    console.log("IF CURRENT WIDTH" + width + "__" + currentDifference);
-                } else {
-                    prevDifference = (width - images[i - 1].width) / 2;
-                    images[i].style.marginLeft = (prevDifference + currentDifference) + "px";
-                    console.log("ELSE CURRENT " + currentDifference);
-
-                }
-
-            }
-
+        if (Slider.imagesCount < 2) {
+            document.querySelector('.slider-next').style.display = "none";
         }
+    }
+
+
+    static initPrevious() {
+        document.querySelector('.slider-prev').addEventListener('mousedown', function () {
+            Slider.doPrev();
+        });
+        if (Slider.imagesCount < 2) {
+            document.querySelector('.slider-prev').style.display = "none";
+        }
+    }
+
+    static doNext() {
+        Slider.position++;
+        if (Slider.position >= Slider.imagesCount) {
+            Slider.position = 0;
+        }
+        Slider.rollSlider();
+    }
+
+    static doPrev() {
+        Slider.position--;
+
+        if (Slider.position < 0) {
+            Slider.position = Slider.imagesCount - 1;
+        }
+        Slider.rollSlider();
+    }
+
+    static rollSlider() {
+        let transWidth = document.querySelector('.slider').offsetWidth;
+        if (!transWidth) {
+            transWidth = document.querySelector('.donate-column-1').offsetWidth;
+        }
+
+        document.querySelectorAll(".slider_img").forEach(node => node.style.width = transWidth + "px");
+
+        Slider.sliderLine.style.transform = 'translate(-' + Slider.position * transWidth + 'px)';
     }
 }
-
-window.onload = () => {
-    setTimeout(() => {
-        const images = document.querySelectorAll('.slider .slider-line img');
-        const sliderLine = document.querySelector('.slider .slider-line');
-        let count = 0;
-        let width;
-
-        function init() {
-            width = document.querySelector('.slider').offsetWidth;
-            rollSlider();
-            imgByCenter();
-        }
-
-        init();
-
-        window.addEventListener('resize', init);
-
-        document.querySelector('.slider-next').addEventListener('click', function () {
-            count++;
-            if (count >= images.length) {
-                count = 0;
-            }
-            rollSlider();
-        });
-        document.querySelector('.slider-prev').addEventListener('click', function () {
-            count--;
-            if (count < 0) {
-                count = images.length - 1;
-            }
-            rollSlider();
-        });
-
-        function rollSlider() {
-            sliderLine.style.transform = 'translate(-' + count * width + 'px)';
-
-        }
-
-        function imgByCenter() {
-            for (let i = 0; i < images.length; i++) {
-                let prevDifference;
-                let currentDifference = (width - images[i].width) / 2;
-                if (images[i - 1] == null) {
-                    images[i].style.marginLeft = currentDifference + "px";
-                } else {
-                    prevDifference = (width - images[i - 1].width) / 2;
-                    images[i].style.marginLeft = (prevDifference + currentDifference) + "px";
-                }
-
-            }
-        }
-    }, 0)
-}
-
-
